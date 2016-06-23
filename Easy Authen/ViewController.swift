7e1a94d23775db4269de1435e5a4a1a6c753b3cd
chanPecko <peckoz@hotmail.com>
2016-06-23 13:43:34 +0700

    UI and Check Data

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
+
+
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var User: UITextField!
+    
+    
+    @IBOutlet weak var Password: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +29,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func LoginButton(sender: AnyObject) {
+        
+        let user = User.text
+        let pass = Password.text
+        
+        print("user ==> \(user)")
+        print("password ==> \(pass))")
+        
+        
+        
+    } // Login
+    
 
 
-}
+} // Class
 
